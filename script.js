ead5ac05c9004312d74bd240a992901122717505
thisisbusinessin-tech <thisisbusinessin-tech@users.noreply.github.com>
2026-03-29 14:17:50 +0000

    deploy: thisisbusinessin-tech/company-website-source@3571a5a93a3e7d448bd8657c54788f09b537ad1f

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const glow = document.getElementById('glow');
-
-    // Listen for mouse movement across the entire document
-    document.addEventListener('mousemove', (e) => {
-        // Update the position of the glow element to follow the cursor
-        glow.style.left = e.clientX + 'px';
-        glow.style.top = e.clientY + 'px';
-    });
-});
+console.log("Website loaded successfully");
